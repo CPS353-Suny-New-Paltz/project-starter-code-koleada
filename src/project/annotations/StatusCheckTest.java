bb63d833ac213a172def65ad14678508ed0895ac
Kole <103860851+koleada@users.noreply.github.com>
2025-09-01 17:12:30 -0400

    fixed StatusCheckTest.java
    i believe it should pass all checkstyle and compilation checks now

---
+++ src/project/annotations/StatusCheckTest.java
@@ -0,0 +1,13 @@
+/**
+ * A test class for status checking.
+ * @author kole
+ */
+public class StatusCheckTest {
+  /**
+   * Main method for testing the application.
+   *
+   * @param args command-line arguments
+   */
+  public static void main(String[] args) {
+  }
+}}
