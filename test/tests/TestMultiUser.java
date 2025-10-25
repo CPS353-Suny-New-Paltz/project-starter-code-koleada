5e930d7f846958b36190d446755129b7967382e2
Kole <103860851+koleada@users.noreply.github.com>
2025-10-25 13:20:30 -0400

    fix checkstyle

--- test/tests/TestMultiUser.java
+++ test/tests/TestMultiUser.java
@@ -45,15 +45,15 @@ public class TestMultiUser {
 
   @Test
   public void compareMultiAndSingleThreaded() throws Exception {
-    int nThreads = 4;
+    int numThreads = 4;
     List<TestUser> testUsers = new ArrayList<>();
-    for (int i = 0; i < nThreads; i++) {
+    for (int i = 0; i < numThreads; i++) {
       testUsers.add(new TestUser(coordinator));
     }
 
     // Run single threaded
     String singleThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.singleThreadOut.tmp";
-    for (int i = 0; i < nThreads; i++) {
+    for (int i = 0; i < numThreads; i++) {
       File singleThreadedOut = new File(singleThreadFilePrefix + i);
       singleThreadedOut.deleteOnExit();
       testUsers.get(i).run(singleThreadedOut.getCanonicalPath());
@@ -63,7 +63,7 @@ public class TestMultiUser {
     ExecutorService threadPool = Executors.newCachedThreadPool();
     List<Future<?>> results = new ArrayList<>();
     String multiThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.multiThreadOut.tmp";
-    for (int i = 0; i < nThreads; i++) {
+    for (int i = 0; i < numThreads; i++) {
       File multiThreadedOut = new File(multiThreadFilePrefix + i);
       multiThreadedOut.deleteOnExit();
       String multiThreadOutputPath = multiThreadedOut.getCanonicalPath();
@@ -81,8 +81,9 @@ public class TestMultiUser {
 
     // Check that the output is the same for multi-threaded and single-threaded
     List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix,
-        nThreads);
-    List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, nThreads);
+        numThreads);
+    List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix,
+        numThreads);
     assertEquals(singleThreaded, multiThreaded);
   }
 
