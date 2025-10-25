dd0206dad2699b3f1dc5855116048c7f23c52110
Kole <103860851+koleada@users.noreply.github.com>
2025-10-24 20:50:16 -0400

    did testuser
    added basic code just to build a request using the input and output files and running the .compute method with that

--- test/tests/TestUser.java
+++ test/tests/TestUser.java
@@ -2,7 +2,12 @@ package tests;
 
 import java.io.File;
 
+import network.api.ComputationRequest;
+import network.api.ComputationResponse;
+import network.api.Delimiter;
 import network.api.NetworkApi;
+import shared.stuff.Resource;
+import shared.stuff.ResourceType;
 
 public class TestUser {
 
@@ -15,11 +20,24 @@ public class TestUser {
   }
 
   public void run(String outputPath) {
-    char delimiter = ';';
+    // char delimiter = ','; NOT NEEDED
     String inputPath = "test" + File.separatorChar + "testInputFile.test";
 
     // TODO 4: Call the appropriate method(s) on the coordinator to get it to
     // run the compute job specified by inputPath, outputPath, and delimiter
+
+    Resource inputResource = new Resource(ResourceType.FILE, inputPath);
+    Resource outputResource = new Resource(ResourceType.FILE, outputPath);
+
+    // delimiter object using ,
+    Delimiter delim = Delimiter.defaultDelimiter();
+
+    // Build request
+    ComputationRequest request = new ComputationRequest(inputResource,
+        outputResource, delim);
+
+    ComputationResponse response = coordinator.compute(request);
+
   }
 
 }
