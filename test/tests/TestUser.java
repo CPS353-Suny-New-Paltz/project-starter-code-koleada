be72056f7105e3f9a5e4a23f6771544f8c4dbebd
Kole <103860851+koleada@users.noreply.github.com>
2025-11-02 15:37:31 -0500

    trying to fix compile error

--- test/tests/TestUser.java
+++ test/tests/TestUser.java
@@ -21,34 +21,57 @@ public class TestUser {
 
   }
 
+  /**
+   * Getting a super weird error on github, its saying testInputFile.test doesnt
+   * exist adn wotn let me merge, locally everything passes with no errors, no
+   * idea why this is happening now
+   * 
+   * public void run(String outputPath) { String projectRoot =
+   * System.getProperty("user.dir"); String inputPath = projectRoot +
+   * File.separator + "test" + File.separator + "testInputFile.test";
+   * 
+   * // TODO 4: Call the appropriate method(s) on the coordinator to get it to
+   * // run the compute job specified by inputPath, outputPath, and delimiter
+   * 
+   * Resource inputResource = new Resource(ResourceType.FILE, inputPath);
+   * Resource outputResource = new Resource(ResourceType.FILE, outputPath);
+   * 
+   * // delimiter object using , Delimiter delim = Delimiter.defaultDelimiter();
+   * 
+   * // Build request ComputationRequest request = new
+   * ComputationRequest(inputResource, outputResource, delim);
+   * 
+   * ComputationResponse response = coordinator.compute(request);
+   * 
+   * if (response.getStatus() != ApiStatus.SUCCESS) {
+   * System.out.println("Compute failed: " + response.getMessage()); }
+   * 
+   * File f = new File(outputPath); System.out.println( "Output exists? " +
+   * f.exists() + ", path = " + f.getAbsolutePath());
+   * 
+   * }
+   */
+
   public void run(String outputPath) {
-    String projectRoot = System.getProperty("user.dir");
-    String inputPath = projectRoot + File.separator + "test" + File.separator
-        + "testInputFile.test";
+    // Load the test input file relative to this class
+    File inputFile = new File("test/testInputFile.test");
+    if (!inputFile.exists()) {
+      inputFile = new File(
+          System.getProperty("user.dir") + "/test/testInputFile.test");
+    }
 
-    // TODO 4: Call the appropriate method(s) on the coordinator to get it to
-    // run the compute job specified by inputPath, outputPath, and delimiter
-
+    String inputPath = inputFile.getAbsolutePath();
     Resource inputResource = new Resource(ResourceType.FILE, inputPath);
     Resource outputResource = new Resource(ResourceType.FILE, outputPath);
 
-    // delimiter object using ,
     Delimiter delim = Delimiter.defaultDelimiter();
-
-    // Build request
     ComputationRequest request = new ComputationRequest(inputResource,
         outputResource, delim);
-
     ComputationResponse response = coordinator.compute(request);
 
     if (response.getStatus() != ApiStatus.SUCCESS) {
       System.out.println("Compute failed: " + response.getMessage());
     }
-
-    File f = new File(outputPath);
-    System.out.println(
-        "Output exists? " + f.exists() + ", path = " + f.getAbsolutePath());
-
   }
 
 }
